335ced700f619d9ce8af1ed327482398ae927496
spark <spark.scul@gmail.com>
2016-11-26 13:45:05 -0500

    execute a command

---
+++ splash.py
@@ -0,0 +1,18 @@
+import os
+
+def prompt():
+  """ Prompt the user for a line"""
+  try:
+    in_string = input("splash$ ")
+    command = in_string.split(' ')[0] 
+    args = in_string.split(' ')
+    return (command, args) 
+  except (NameError, SyntaxError):
+    print("Invalid input. \n")
+
+
+while (True):
+  (command, args) = prompt()
+  # print("command: %s \nargs: %s" % (command, args))
+  os.execvp(command, args) 
+
